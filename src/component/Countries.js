629fe2d42526222c075053751878fe75fd396447
sonusubedi-qa <sonusubedi.qa@gmail.com>
2023-08-23 21:28:16 -0400

    display countries using API data

--- src/component/Countries.js
+++ src/component/Countries.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
 import Country from './Country';
+
 function Countries() {
-    return <Country/>;
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                setCountries(data);
+                console.log(data);
+            })
+            .catch(error => {
+                console.log("Error in post data", error);
+
+            })
+    }, [])
+
+
+    return <div class="d-flex flex-wrap gap-3 justify-content-evenly">
+        {
+            countries.map(country => <Country flagSrc={country.flags.png} name={country.name.common} shortDescription={country.flags.alt} />)
+        }
+    </div>
 }
 export default Countries;
--- src/component/Country.js
+++ src/component/Country.js
@@ -1,5 +1,11 @@
 import '../App.css';
-function Country() {
-    return <p>Country</p>;
+function Country(props) {
+  return <div class="card col-6 col-sm-4 col-md-3 mb-4">
+      <img src={props.flagSrc} class="card-img-top img-fluid" alt=""/>
+    <div class="card-body">
+      <h5 class="card-title">{props.name}</h5>
+      <p class="card-text">{props.shortDescription}</p>
+    </div>
+  </div>
 }
 export default Country;
