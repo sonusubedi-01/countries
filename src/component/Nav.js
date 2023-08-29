59206cdafd0bcfea11ecab2034c6b6917a865070
sonusubedi-qa <sonusubedi.qa@gmail.com>
2023-08-28 22:35:30 -0400

    fix href build issue in Nav Component

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -4,13 +4,13 @@ import Theme from './Theme';
 function Nav() {
   return <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 mt-3">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Countries</a>
+      <a class="navbar-brand" href="/">Countries</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link" href="#">Favorites</a>
+          <a class="nav-link" href="/">Favorites</a>
         </div>
       </div>
     </div>
