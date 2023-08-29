d5fb4bc633589fd77fcb882b86dec30ce0b72bbf
sonusubedi-qa <sonusubedi.qa@gmail.com>
2023-08-28 21:47:59 -0400

    Updates Navbar

--- src/component/Country.js
+++ src/component/Country.js
@@ -1,6 +1,6 @@
 import '../App.css';
 function Country(props) {
-  return <div className="card col-6 col-sm-4 col-md-3 mb-4">
+  return <div className="card col-10 col-sm-4 col-md-2 mb-4">
       <img src={props.flagSrc} className="card-img-top img-fluid" alt=""/>
     <div className="card-body">
       <h5 className="card-title">{props.name}</h5>
